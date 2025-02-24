d0fcf719ebde38a7411658ccb6b4b61b0a8b71e8
Tomas Jelvez <42525910+tomasjelvez@users.noreply.github.com>
2025-02-24 19:47:40 -0300

    fix: CORS

--- scraping-service/src/index.ts
+++ scraping-service/src/index.ts
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
     methods: ["GET"],
   })
 );
